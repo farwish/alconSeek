73b113a2f5b12db9667ccc7168c78ee5de1e81bf
farwish <farwish@foxmail.com>
2016-11-23 16:45:20 +0800

    config and deploy

--- app/config/config.php
+++ app/config/config.php
@@ -2,23 +2,6 @@
 
 defined('APP_PATH') || define('APP_PATH', realpath('.'));
 
-return new \Phalcon\Config(array(
-    'database' => array(
-        'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '123456',
-        'dbname'      => 'speed_dev',
-        'charset'     => 'utf8',
-    ),
-    'application' => array(
-        'controllersDir' => APP_PATH . '/app/controllers/',
-        'modelsDir'      => APP_PATH . '/app/models/',
-        'migrationsDir'  => APP_PATH . '/app/migrations/',
-        'viewsDir'       => APP_PATH . '/app/views/',
-        'pluginsDir'     => APP_PATH . '/app/plugins/',
-        'libraryDir'     => APP_PATH . '/app/library/',
-        'cacheDir'       => APP_PATH . '/app/cache/',
-        'baseUri'        => '/Speed/',
-    )
-));
+return new \Phalcon\Config\Adapter\Ini(
+    APP_PATH . '/app/config/config.ini'
+);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# speed
+
+> 应用依赖: Xunsearch服务
+
+> 首次运行: ./deploy
